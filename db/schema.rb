a636d707026c3605bb290b86464b70c547c54c67
minhthinh1612 <minhthinh1995@gmail.com>
2019-05-20 22:17:15 +0700

    Create Answer, Question model

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,25 @@
 
 ActiveRecord::Schema.define(version: 20190521021109) do
 
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.text "content"
+    t.boolean "is_correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "category_id"
+    t.text "content"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
