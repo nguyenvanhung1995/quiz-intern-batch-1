c4aa180d7cdc30a11fe225430c40eed8ff510882
nguyenvanhung1995 <ghunx1@gmail.com>
2019-05-21 15:30:57 +0700

    Task_9 Category index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,5 @@
 class CategoriesController < ApplicationController
-
-	def new
-		@category = Category.new
-	end
-
-	def index
-		@categories = Category.all
-	end
-
-	def show
-		@category = Category.find_by(id: params[:id])
-	end
-
-	def create
-		@category=Category.new(category_params)
-		if @category.save
-			flash[:success] = "Added new Category success !"
-			redirect_to categories_path
-
-		else
-			render 'new'
-		end
-	end
-
-	private
-
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  def index
+    @categories = Category.all
+  end
 end
