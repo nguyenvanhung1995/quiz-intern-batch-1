8a6024237b769b2e674354207a534c48506ff6b2
nguyenvanhung1995 <ghunx1@gmail.com>
2019-05-21 11:35:11 +0700

    Category index page

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+
+	def new
+		@category = Category.new
+	end
+
+	def index
+		@categories = Category.all
+	end
+
+	def show
+		@category = Category.find_by(id: params[:id])
+	end
+	def create
+		@category=Category.new(category_params)
+		if @category.save
+			flash[:success] = "Added success!"
+			redirect_to @category
+		else
+			render 'new'
+		end
+	end
+	private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
