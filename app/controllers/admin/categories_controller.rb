1798586eda4f45a8c055e7945e590a095b4eb071
nguyenvanhung1995 <ghunx1@gmail.com>
2019-05-23 11:20:20 +0700

    Task_10 Delete, insert and update category : fresher code

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,12 @@
 class Admin::CategoriesController < ApplicationController
-  
   def index
     @categories = Category.all
   end
+
   def new
     @category = Category.new
   end
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -15,27 +16,30 @@ class Admin::CategoriesController < ApplicationController
       render 'new'
     end
   end
+
   def edit
     @category = Category.find(params[:id])
   end
+
   def update
     @category = Category.find(params[:id])
+    @category_old_name = @category.name
     if @category.update_attributes(category_params)
-      flash[:success] = "Update category's name success!"
+      flash[:success] = "Changed from #{@category_old_name}  to new name #{@category.name} success!"
       redirect_to admin_categories_path
     else
       render 'edit'
     end
   end
+
   def destroy
     Category.find(params[:id]).destroy
     flash[:success] = "Category deleted"
     redirect_to admin_categories_path
   end
-  # def show
-  #   @category = Category.find(params[:id])
-  # end
+  
   private
+
   def category_params
     params.require(:category).permit(:name)
   end
