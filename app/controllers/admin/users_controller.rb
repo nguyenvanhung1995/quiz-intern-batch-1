7efbc0ce039ad95a1f111ff5521e3c59a971c809
nguyenvanhung1995 <ghunx1@gmail.com>
2019-05-27 08:12:18 +0700

    Task 20 Delete, update, insert user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,50 @@
 class Admin::UsersController < ApplicationController
-  before_action :find_user, only: [:show]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end
 
   def show; end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Added new user : #{@user.name} success !"
+      redirect_to admin_users_path
+    else
+      render "new"
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update success !"
+      redirect_to admin_users_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted "
+    redirect_to admin_users_path
+  end
+
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit :name, :birthday, :address, :email, :role, :password
   end
 
 end
