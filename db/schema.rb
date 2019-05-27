f3399e2ab3842d3698a4158dc5babb542a4c8af7
nguyenvanhung1995 <ghunx1@gmail.com>
2019-05-27 11:11:54 +0700

    Task_20 : fresher code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190525082427) do
+ActiveRecord::Schema.define(version: 20190527034443) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20190525082427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "image"
   end
 
 end
